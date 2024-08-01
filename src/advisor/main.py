85dbcb6af680eda563af67160bf5dd14e442c8d9
Maxwell Ward <maxward4@gmail.com>
2024-08-01 18:09:16 -0400

    refactor: Improve allocation balancing and printing in robinhood.py

--- src/advisor/main.py
+++ src/advisor/main.py
@@ -1,18 +1,18 @@
-from advisor.allocate import get_stock_allocations
+from advisor.allocate import get_stock_allocations, run
 from advisor.robinhood import get_positions, compare_allocations_to_positions, allocate_new_investment
 
 if __name__ == "__main__":
-    # print('get_stock_allocations')
-    # print(get_stock_allocations())
-    # print()
+    print('get_stock_allocations')
+    print(get_stock_allocations())
+    print()
 
-    # print('robinhood.py get_positions')
-    # print(get_positions())
-    # print()
+    print('robinhood.py get_positions')
+    print(get_positions())
+    print()
 
     # print('compare_allocations_to_positions')
     # print(compare_allocations_to_positions())
     # print()
 
     print('allocate_new_investment')
-    print(allocate_new_investment())
+    allocate_new_investment()
